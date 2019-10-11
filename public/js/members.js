01555c3cb0346a3bbf88bc79ee12b0be42998246
Rita Manley <manley.rita@gmail.com>
2019-10-10 21:21:52 -0700

    FB share button on finance articles

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  
       // This file just does a GET request to figure out which user is logged in
       // and updates the HTML on the page
       $.get("/api/user_data").then(function (data) {
@@ -72,6 +73,7 @@ $(document).ready(function () {
 
 
         $("#financial-news").on("click", function () {
+         
 
           var url2 = 'https://newsapi.org/v2/everything?' +
             'q=retirement&' +
@@ -96,13 +98,28 @@ $(document).ready(function () {
             // .then(articles => console.log(articles["0"].author))
             .then(articles => {
               for (let i = 0; i < articles.length; i++) {
-                 var $newDiv = $("<div id='shareGoodNews' class='modal - footer fb - share - button' data-href=" + articles[i].url + "data - layout = 'button_count' > < /div>")
+                //  var $newDiv = $("<div id='shareGoodNews' class='modal-footer fb-share-button' data-href=" + articles[i].url + "data-layout='button_count'> </div>")
+
+                var $newDiv =$("<div id='shareGoodNews' class='fb-share-button' data-href='https://www.your-domain.com/your-page.html' data-layout='button_count'>FB </div>");
+{/* <div class="fb-share-button" 
+    data-href="https://www.your-domain.com/your-page.html" 
+    data-layout="button_count">
+  </div> */}
                 
                 console.log($newDiv);
                 $newDiv.appendTo("#financeArticles");
                $("#financeArticles").append("<a href='" + articles[i].url + "'>" + articles[i].title + "</a>" +
                  $newDiv + "<br>");
               }
+                
+              (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
             })
         })
       });
