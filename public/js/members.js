231218afa5c212780c1acb1b35e7e1b2a7b6c6a7
Rita Manley <manley.rita@gmail.com>
2019-10-18 21:14:57 -0700

    removed twitter buttons

--- public/js/members.js
+++ public/js/members.js
@@ -5,117 +5,100 @@
   });
 
 
-//newsapi.org
-$("#good-news").click(function () {
-  $("#goodArticles").empty();
+  //newsapi.org
+  $("#good-news").click(function () {
+    $("#goodArticles").empty();
 
-  var url = 'https://newsapi.org/v2/everything?' +
-    'q=happy AND elderly&' +
-    'sortBy=popularity&' +
-    'pageSize=3&' +
-    'sources=associated-press,abc-news,cnn,the-washington-post,the-washington-times,the-new-york-times,msnbc,cbs-news,medical-news-today,nbc-news,newsweek,the-wall-street-journal,new-york-magazine,google-news&' +
-    'apiKey=0642787cf5e744f68ecb6d77e9c99efb';
+    var url = 'https://newsapi.org/v2/everything?' +
+      'q=happy AND elderly NOT fiction&' +
+      'sortBy=popularity&' +
+      'pageSize=3&' +
+      'sources=associated-press,abc-news,cnn,the-washington-post,the-washington-times,the-new-york-times,msnbc,cbs-news,medical-news-today,nbc-news,newsweek,the-wall-street-journal,new-york-magazine,google-news&' +
+      'apiKey=0642787cf5e744f68ecb6d77e9c99efb';
 
-  var req = new Request(url);
-  fetch(req)
-    .then(resp => resp.json())
-    // Make sure to return articles from this function after logging it
-    .then(({
-      articles
-    }) => {
-      return articles;
-    })
-    // Take the articles as an argument, then log the author property of the first entry.
-    // .then(articles => console.log(articles["0"].author))
-    .then(articles => {
-      for (let i = 0; i < articles.length; i++) {
-        var $newDiv = $('<div class="fb-share-button" data-href="'+articles[i].url+'" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='+articles[i].url+'&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to Facebook</a></div>');
+    var req = new Request(url);
+    fetch(req)
+      .then(resp => resp.json())
+      .then(({
+        articles
+      }) => {
+        return articles;
+      })
 
-        var $twitterDiv = $('<a href="'+articles[i].url+'" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
+      .then(articles => {
+        for (let i = 0; i < articles.length; i++) {
+          var $newDiv = $('<class="fb-share-button" data-href="' + articles[i].url + '" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' + articles[i].url + '&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to Facebook</a>');
 
-        $("#goodArticles").append($newDiv);
+          $("#goodArticles").append($newDiv);
 
-        $("#goodArticles").append($twitterDiv);
-
-        $("#goodArticles").append("<img class='linkPic' src='" + articles[i].urlToImage+ "'>" + "<a href='" + 
-        articles[i].url + "'>" + articles[i].title + "</a><br><br><br>");
-      }
-    })
-});
+          $("#goodArticles").append("<img class='linkPic' src='" + articles[i].urlToImage + "'>" + "<a href='" +
+            articles[i].url + "'>" + articles[i].title + "</a><br><br><br>");
+        }
+      })
+  });
 
 
 
-$("#financial-news").on("click", function () {
-  $("#financeArticles").empty();
+  $("#financial-news").on("click", function () {
+    $("#financeArticles").empty();
 
-  var url2 = 'https://newsapi.org/v2/everything?' +
-    'q=finance AND elderly&' +
-    'sortBy=popularity&' +
-    'pageSize=3&' +
-    'sources=associated-press,abc-news,cnn,the-washington-post,the-washington-times,the-new-york-times,msnbc,cbs-news,medical-news-today,nbc-news,newsweek,the-wall-street-journal,new-york-magazine,google-news&' +
-    'apiKey=0642787cf5e744f68ecb6d77e9c99efb';
+    var url2 = 'https://newsapi.org/v2/everything?' +
+      'q=finance AND elderly NOT Hong &' +
+      'sortBy=popularity&' +
+      'pageSize=3&' +
+      'sources=associated-press,abc-news,cnn,the-washington-post,the-washington-times,the-new-york-times,msnbc,cbs-news,medical-news-today,nbc-news,newsweek,the-wall-street-journal,new-york-magazine,google-news&' +
+      'apiKey=0642787cf5e744f68ecb6d77e9c99efb';
 
-  var req2 = new Request(url2);
-  fetch(req2)
-    .then(resp => resp.json())
-    .then(({
-      articles
-    }) => {
-      return articles;
-    })
-    .then(articles => {
-      for (let i = 0; i < articles.length; i++) {
-        var $newDiv = $('<div class="fb-share-button" data-href="'+articles[i].url+'" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='+articles[i].url+'&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to Facebook</a></div>');
+    var req2 = new Request(url2);
+    fetch(req2)
+      .then(resp => resp.json())
+      .then(({
+        articles
+      }) => {
+        return articles;
+      })
+      .then(articles => {
+        for (let i = 0; i < articles.length; i++) {
+          var $newDiv = $('<class="fb-share-button" data-href="' + articles[i].url + '" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' + articles[i].url + '&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to Facebook</a>');
 
-        var $twitterDiv = $('<a href="'+articles[i].url+'" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
+          $("#financeArticles").append($newDiv);
 
-        $("#financeArticles").append($newDiv);
-
-        $("#financeArticles").append($twitterDiv);
-
-        $("#financeArticles").append("<img class='linkPic' src='" + articles[i].urlToImage+ "'>" + "<a href='" + 
-        articles[i].url + "'>" + articles[i].title + "</a><br><br><br>");
-      }
-    })
-});
+          $("#financeArticles").append("<img class='linkPic' src='" + articles[i].urlToImage + "'>" + "<a href='" +
+            articles[i].url + "'>" + articles[i].title + "</a><br><br><br>");
+        }
+      })
+  });
 
 
-$("#health-news").click(function () {
-  $("#healthArticles").empty();
+  $("#health-news").click(function () {
+    $("#healthArticles").empty();
 
+    var url1 = 'https://newsapi.org/v2/everything?' +
+      'q=elderly AND health NOT rioting &' +
+      'sortBy=popularity&' +
+      'pageSize=3&' +
+      'sources=associated-press,abc-news,cnn,the-washington-post,the-washington-times,the-new-york-times,msnbc,cbs-news,medical-news-today,nbc-news,newsweek,the-wall-street-journal,new-york-magazine,google-news&' +
+      'apiKey=0642787cf5e744f68ecb6d77e9c99efb';
 
-  var url1 = 'https://newsapi.org/v2/everything?' +
-    'q=elderly AND healthy&' +
-    'sortBy=popularity&' +
-    'pageSize=3&' +
-    'sources=associated-press,abc-news,cnn,the-washington-post,the-washington-times,the-new-york-times,msnbc,cbs-news,medical-news-today,nbc-news,newsweek,the-wall-street-journal,new-york-magazine,google-news&' +
-    'apiKey=0642787cf5e744f68ecb6d77e9c99efb';
+    var req1 = new Request(url1);
+    fetch(req1)
+      .then(resp => resp.json())
+      .then(({
+        articles
+      }) => {
+        // console.log(articles);
+        return articles;
+      })
 
-  var req1 = new Request(url1);
-  fetch(req1)
-    .then(resp => resp.json())
-    // Make sure to return articles from this function after logging it
-    .then(({
-      articles
-    }) => {
-      // console.log(articles);
-      return articles;
-    })
-    // Take the articles as an argument, then log the author property of the first entry.
-    // .then(articles => console.log(articles["0"].author))
-    .then(articles => {
-      for (let i = 0; i < articles.length; i++) {
-        var $newDiv = $('<div class="fb-share-button" data-href="'+articles[i].url+'" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='+articles[i].url+'&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to Facebook</a></div>');
+      .then(articles => {
+        for (let i = 0; i < articles.length; i++) {
+          var $newDiv = $('<class="fb-share-button" data-href="' + articles[i].url + '"data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' + articles[i].url + '&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to Facebook</a>');
 
-        var $twitterDiv = $('<a href="'+articles[i].url+'" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
+          $("#healthArticles").append($newDiv);
 
-        $("#healthArticles").append($newDiv);
+          $("#healthArticles").append("<img class='linkPic' src='" + articles[i].urlToImage + "'>" + "<a href='" +
+            articles[i].url + "'>" + articles[i].title + "</a><br><br><br>");
 
-        $("#healthArticles").append($twitterDiv);
-
-        $("#healthArticles").append("<img class='linkPic' src='" + articles[i].urlToImage+ "'>" + "<a href='" + 
-        articles[i].url + "'>" + articles[i].title + "</a><br><br><br>");
-      }
-    })
-});
-
+        }
+      })
+  });
